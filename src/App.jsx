24febe8a4047f4c75bb33a4831eb96582acae691
shubhambendkhale77 <shubhambendkhale77@gmail.com>
2025-02-06 12:44:43 +0530

    add SignUp and Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import Login from "./assets/Auth/Login";
-import Register from "./assets/Auth/register";
-import Navbar from "./components/Navbar";
-import AppRoutes from "./routers/AppRoutes";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <AppRoutes /> 
 
     </>
   );
